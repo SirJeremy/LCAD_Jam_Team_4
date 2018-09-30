06755d0526f18c80e70666c489cf2261eca0091f
deshowiz <shawn.condon@yahoo.com>
2018-09-30 12:09:54 -0700

    Started on Interactables

---
+++ Assets/Scripts/TerrainPieces/Door.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour, IInteractable
+{
+    [SerializeField] int swingDirection = 1;
+    [SerializeField] float swingSpeed = 1;
+    [SerializeField] int stepLimit = 0;
+    public void Interact()
+    {
+        // Open
+        StartCoroutine(SwingOpen());
+    }
+
+
+    void Start()
+    {
+        StartCoroutine(SwingOpen());
+    }
+
+    IEnumerator SwingOpen()
+    {
+        int steps = 0;
+        while (stepLimit >= steps)
+        {
+            steps++;
+            transform.eulerAngles += new Vector3(0, swingSpeed * swingDirection * Time.deltaTime, 0);
+            yield return null;
+        }
+        
+    }
+}
