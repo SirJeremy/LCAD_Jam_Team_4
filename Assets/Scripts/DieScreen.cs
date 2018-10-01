c7ca79688381e7a7bfdd37701b226edf4123a9a2
Jeremy Cortez <sirjeremycortez@gmail.com>
2018-09-30 19:50:40 -0700

    Added die

---
+++ Assets/Scripts/DieScreen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DieScreen : MonoBehaviour {
+    [SerializeField]
+    private GameObject dieScreen;
+
+
+	// Use this for initialization
+	void Start () {
+        dieScreen.SetActive(false);
+        Character.OnDeath += OnDeath;
+	}
+
+    private void OnDeath() {
+        dieScreen.SetActive(true);
+    }
+}
