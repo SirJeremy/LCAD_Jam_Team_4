137e3570e84380e4195b1f4f77792b7f9612596a
deshowiz <shawn.condon@yahoo.com>
2018-09-30 18:08:19 -0700

    Worked on door

--- Assets/Scripts/TerrainScripts/Door.cs
+++ Assets/Scripts/TerrainScripts/Door.cs
@@ -7,9 +7,23 @@ public class Door : MonoBehaviour, IInteractable
     [SerializeField] int swingDirection = 1;
     [SerializeField] float swingSpeed = 1;
     [SerializeField] int stepLimit = 0;
+    public bool[] conditions;
+    private bool canOpen = false;
     public void Interact()
     {
-        StartCoroutine(SwingOpen());
+        canOpen = true;
+        for (int i = 0; i < conditions.Length; i++)
+        {
+            if (!conditions[i])
+            {
+                canOpen = false;
+                break;
+            }
+        }
+        if (canOpen)
+        {
+            StartCoroutine(SwingOpen());
+        }
     }
 
 
