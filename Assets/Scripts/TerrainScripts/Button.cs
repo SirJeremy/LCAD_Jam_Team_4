89ba327de0e41a4b9896fbcdcbe279168d48a6c1
deshowiz <shawn.condon@yahoo.com>
2018-09-30 18:52:18 -0700

    conveyors

--- Assets/Scripts/TerrainScripts/Button.cs
+++ Assets/Scripts/TerrainScripts/Button.cs
@@ -8,9 +8,14 @@ public class Button : MonoBehaviour, IUseable {
 
     private bool pressed = false;
 
+    [SerializeField]
+    private Door endDoor;
+    [SerializeField]
+    private int conditionNum;
     public void Use() {
         IInteractable interactable = iInteractable.GetComponent<IInteractable>();
         if (interactable != null) {
+            endDoor.conditions[conditionNum] = true;
             pressed = !pressed;
             interactable.Interact(pressed);
         }
